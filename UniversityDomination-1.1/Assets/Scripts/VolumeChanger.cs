86049675786b87e7f82abb06728be2be098ba826
Matt Horder <matt.horder@outlook.com>
2018-02-17 20:50:22 +0000

    Global Volume Slider

--- UniversityDomination-1.1/Assets/Scripts/VolumeChanger.cs
+++ UniversityDomination-1.1/Assets/Scripts/VolumeChanger.cs
@@ -6,10 +6,9 @@ using UnityEngine.UI;
 public class VolumeChanger : MonoBehaviour {
 
     public Slider Volume;
-    public AudioSource backgorundMusic;
-	
+   
 	// Update is called once per frame
 	void Update () {
-        backgorundMusic.volume = Volume.value; // set music volume to value of slider
+        AudioListener.volume = Volume.value; // set music volume to value of slider
 	}
 }
