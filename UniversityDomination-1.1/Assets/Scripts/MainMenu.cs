346ac5c7814f2869b970a9e8be3f02e7036f6849
Matt Horder <matt.horder@outlook.com>
2018-02-17 18:56:13 +0000

    Options Menu

--- UniversityDomination-1.1/Assets/Scripts/MainMenu.cs
+++ UniversityDomination-1.1/Assets/Scripts/MainMenu.cs
@@ -2,22 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class MainMenu : MonoBehaviour {
 
 	public void PlayGame() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // Load next scene in build order
         
     }
 
     public void QuitGame() {
 
-        Debug.Log("Quit");
+        Debug.Log("Quit"); // For Testing
         Application.Quit();
 
     }
 
-		
+	
 }
 
---
+++ UniversityDomination-1.1/Assets/Scripts/VolumeChanger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeChanger : MonoBehaviour {
+
+    public Slider Volume;
+    public AudioSource backgorundMusic;
+	
+	// Update is called once per frame
+	void Update () {
+        backgorundMusic.volume = Volume.value; // set music volume to value of slider
+	}
+}
